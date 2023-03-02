1012759bcfede59fdaef3d5da418080a013ccae3
Afurete233 <diffusesound@yahoo.co.jp>
2023-03-02 16:30:22 +0800

    update

--- src/main.js
+++ src/main.js
@@ -4,13 +4,13 @@ import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue';
 import VueDND from 'awe-dnd'
 import './styles.scss'
-import Bmob from "hydrogen-js-sdk";
+// import Bmob from "hydrogen-js-sdk";
 import Cookies from 'js-cookie'
 import 'gitalk/dist/gitalk.css'
 import "babel-polyfill";
 
-Bmob.initialize("4c0ae9b3a1fbc439", "123456");
-Vue.prototype.Bmob = Bmob
+// Bmob.initialize("4c0ae9b3a1fbc439", "123456");
+// Vue.prototype.Bmob = Bmob
 Vue.prototype.Cookies = Cookies
 
 Vue.use(ElementUI)
